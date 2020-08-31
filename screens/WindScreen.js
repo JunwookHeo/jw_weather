391072c878b17934d6783a48cda18111b191c1cb
junwookheo <junwookheo@gmail.com>
2020-09-01 07:40:31 +1000

    Fixed wind ui.
     Changes to be committed:
    	modified:   screens/WindScreen.js

--- screens/WindScreen.js
+++ screens/WindScreen.js
@@ -2,8 +2,6 @@ import * as React from "react";
 import { useState, useEffect } from 'react';
 import { View, Text, Button,StyleSheet, Image } from "react-native";
 
-import { WebView } from 'react-native-webview';
-
 export default function WindScreen({ route, navigation }) {
   const wind = route.params.wind
   const rotate = `${wind.deg}deg`
@@ -11,7 +9,8 @@ export default function WindScreen({ route, navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.wind}>{wind.speed}[m/s], {wind.deg}[Degrees]</Text>
-      <Image style={{...styles.image, transform: [{ rotate: rotate}]}} source={require('../resource/jon-tyson-1E1J2qGB0G8-unsplash.jpg')}></Image>
+      <Image style={{...styles.image, transform: [{ rotate: rotate}]}} 
+          source={require('../resource/jon-tyson-1E1J2qGB0G8-unsplash.jpg')}></Image>
     </View>
   );
 }
@@ -20,16 +19,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
 
   },
   wind: {
     fontSize: 20,
+    marginTop: 20,
   },
   image: {
-    flex: 1,
-    // width: null,
-    // height: null,
-    resizeMode: 'contain',
-}
+    width: 300,
+    height: 300,
+    borderRadius: 300 / 2,
+    marginTop: 20,
+  },
 });
